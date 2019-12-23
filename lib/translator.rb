3a5053300b166d14192f2461bf38cb70b9e4ea75
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-23 04:54:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,12 +2,17 @@
 require 'yaml'
 
 def load_library(yaml_file)
-  
-  
+  response = {"get_meaning" => {}, "get_emoticon" => {}}
+  library = YAML.load_file(yaml_file)
+  library.each do |trans, emos|
+    response["get_meaning"][emos[1]] = trans
+    response["get_emoticon"][emos[0]] = emos[1]
+  end
+    response
 end
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
 def get_english_meaning
