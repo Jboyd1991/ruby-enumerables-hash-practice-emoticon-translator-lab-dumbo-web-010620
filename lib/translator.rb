24afaa087f0ccfdeba5f3ddfda6a4d7e1180b8d5
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-23 04:55:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,5 +27,16 @@ def get_japanese_emoticon(yaml_file, emoticon)
 end
 
 def get_english_meaning
-  
+  library = load_library(yaml_file)
+  response = nil
+  library["get_meaning"].each do |emo, trans|
+    if emo == emoticon
+      response = trans
+    end
+  end
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
 end
